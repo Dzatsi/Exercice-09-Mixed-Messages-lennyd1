3a0802f491bb949ccd52f899e3bb6b92c3c5b16e
lenny dzatsi <lennydzatsi8@gmail.com>
2023-07-27 11:43:50 +0200

    initializes variables

---
+++ index.js
@@ -0,0 +1,5 @@
+const sujet = ['Blague', 'Inspiration', 'Astrologie']
+const blague = ["Il est impossible de toucher sa lèvre inférieure avec sa langue", "Peux-tu éloigner un peu plus la bouche quand tu parles au téléphone? Merci ?"," Dans ce monde il existe plus de 25250365872056 paresseux et tu en fais partie parce que tu n'as même pas essayé de lire ces chiffres."]
+const inspiration = ["Rappelez-vous que personne ne peut vous faire sentir inférieur sans votre consentement", "La vie est 10% ce qui m'arrive et 90% de comment je réagis à elle", "Croyez en vous-même et tout devient possible.", "la foi sauve"]
+const astrologie = ["vous allez avoir une journée chançeuse aujourd'hui", "vous allez avoir une nuit magnifique", "tu seras riche"]
+console.log(blague[2]);
